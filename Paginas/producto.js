e483c0afb34d872cfe0f3dceb6432c9067eb5ad2
AlanAndres255 <perezlozanoalanandres@gmail.com>
2024-10-25 18:50:25 -0600

    add

--- Paginas/producto.js
+++ Paginas/producto.js
@@ -58,13 +58,65 @@ const handleOnClick = () => {
 };
 if (producto) {
   $("#producto-info").html(`
-                <img src="${producto.imgURL}" alt="${producto.nombre}" style="width: 300px; height: auto;">
-                <h2>${producto.nombre}</h2>
-                <p>Precio: ${producto.precio}</p>
-                <p>${producto.descripcion}</p>
-<button onclick="handleOnClick()" class="btn btn-primary">Agregar al carro</button>
-                <a href="./tienda.html">Volver a la tienda</a>
-            `);
+    <main class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md-6 mb-4 mb-md-0">
+                <img src="${producto.imgURL}" alt="${producto.imgURL}" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-6">
+                <p class="text-danger">Sustainable materials</p>
+                <h1 class="mb-5"><b>${producto.nombre}</b></h1>
+                <p class="mb-3">${producto.precio}</p>
+                <p class="mb-5">${producto.descripcion}</p>
+                <p class="mb-5">Selecciona tu talla</p>
+                <div class="row g-1 mb-2">
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_22" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_22">CM 22</label>
+                    </div>
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_23" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_23">CM 23</label>
+                    </div>
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_24" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_24">CM 24</label>
+                    </div>
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_25" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_25">CM 25</label>
+                    </div>
+                </div>
+                <div class="row g-1 mb-4">
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_26" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_26">CM 26</label>
+                    </div>
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_27" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_27">CM 27</label>
+                    </div>
+                    <div class="col-3">
+                        <input type="radio" class="btn-check" name="options" id="CM_28" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_28">CM 28</label>
+                    </div>
+                    <div class="col-3 bt-5">
+                        <input type="radio" class="btn-check" name="options" id="CM_29" autocomplete="off">
+                        <label class="btn btn-outline-dark btn-sm w-100" for="CM_29">CM 29</label>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-4">
+                        <a href="./tienda.html" class="btn btn-dark w-70">Regresar a la tienda</a>
+                    </div>
+                    <div class="col-4">
+                        <a href="" class="btn btn-dark w-70">Agregar a carrito</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+                `);
 } else {
   $("#producto-info").html(`<p>Producto no encontrado.</p>`);
 }
