58ae8181e8fe1349e9f4df115d821caa7ed73463
Roberto RG <robertoromeroguadarrama@gmail.com>
2024-10-26 13:26:58 -0600

    localStorage.clear()

--- Paginas/render-productos.js
+++ Paginas/render-productos.js
@@ -1,5 +1,5 @@
 const data = [
- {
+  {
     id: 1,
     nombre: "Nike Journey Run",
     precio: "$2,099",
@@ -28,50 +28,61 @@ const data = [
     id: 4,
     nombre: "Nike Pegasus Plus",
     precio: "$4,199",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a8a36278-bdd7-47d6-9e6c-201d3e0820bb/PEGASUS+PLUS.png",
-    descripcion: "Innovación y estilo se combinan en este zapato de alto rendimiento."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a8a36278-bdd7-47d6-9e6c-201d3e0820bb/PEGASUS+PLUS.png",
+    descripcion:
+      "Innovación y estilo se combinan en este zapato de alto rendimiento.",
   },
   {
     id: 5,
     nombre: "Nike Rival Fly 4",
     precio: "$2,399",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/86a9258e-acac-4ea5-942d-c65614109c1b/NIKE+AIR+ZOOM+RIVAL+FLY+4.png",
-    descripcion: "Diseñados para la velocidad, estos zapatos te llevarán más lejos."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/86a9258e-acac-4ea5-942d-c65614109c1b/NIKE+AIR+ZOOM+RIVAL+FLY+4.png",
+    descripcion:
+      "Diseñados para la velocidad, estos zapatos te llevarán más lejos.",
   },
   {
     id: 6,
     nombre: "Jordan Heir 'Bloodline'",
     precio: "$2,699",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f082446b-4aed-40cd-b4b3-b8ccccd90c66/JORDAN+HEIR.png",
-    descripcion: "Un diseño icónico que rinde homenaje a la herencia de Jordan."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f082446b-4aed-40cd-b4b3-b8ccccd90c66/JORDAN+HEIR.png",
+    descripcion:
+      "Un diseño icónico que rinde homenaje a la herencia de Jordan.",
   },
   {
     id: 7,
     nombre: "Air Jordan 1 Low SE",
     precio: "$3,249",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1c897d92-a0ce-488e-bb2a-5e7e766d7614/AIR+JORDAN+1+LOW+SE.png",
-    descripcion: "Estilo clásico y comodidad moderna en cada paso."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/1c897d92-a0ce-488e-bb2a-5e7e766d7614/AIR+JORDAN+1+LOW+SE.png",
+    descripcion: "Estilo clásico y comodidad moderna en cada paso.",
   },
   {
     id: 8,
     nombre: "Air Jordan Dub Zero",
     precio: "$3,999",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c264faee-3451-4d38-963f-e224782fbb42/AIR+JORDAN+DUB+ZERO.png",
-    descripcion: "Tecnología avanzada y diseño audaz para los amantes del baloncesto."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c264faee-3451-4d38-963f-e224782fbb42/AIR+JORDAN+DUB+ZERO.png",
+    descripcion:
+      "Tecnología avanzada y diseño audaz para los amantes del baloncesto.",
   },
   {
     id: 9,
     nombre: "Air Jordan 1 Zoom CMFT 2",
     precio: "$3,599",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/4d6e54aa-7104-47c6-8777-3d838d6980b3/AIR+JORDAN+1+ZM+AIR+CMFT+2.png",
-    descripcion: "Comodidad y estilo se unen en este modelo clásico renovado."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/4d6e54aa-7104-47c6-8777-3d838d6980b3/AIR+JORDAN+1+ZM+AIR+CMFT+2.png",
+    descripcion: "Comodidad y estilo se unen en este modelo clásico renovado.",
   },
   {
     id: 10,
     nombre: "Air Jordan 1 Mid",
     precio: "$3,199",
-    imgURL: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/9921d678-ed65-49c6-bdcb-e85bebeb7c46/AIR+JORDAN+1+MID.png",
-    descripcion: "Un clásico atemporal que nunca pasa de moda."
+    imgURL:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/9921d678-ed65-49c6-bdcb-e85bebeb7c46/AIR+JORDAN+1+MID.png",
+    descripcion: "Un clásico atemporal que nunca pasa de moda.",
   },
 ];
 
@@ -109,7 +120,7 @@ $(document).ready(() => {
             </div>
           </div>
           <div class="d-flex align-items-center justify-content-center">
-            <button data-id="${producto.id}" class="btn-eliminar btn btn-danger">Eliminar</button>
+            <button onclick="localStorage.clear()" data-id="${producto.id}" class="btn-eliminar btn btn-danger">Eliminar</button>
           </div>
         </div>`;
     });
@@ -164,3 +175,4 @@ $(document).ready(() => {
 
   renderProductos();
 });
+
