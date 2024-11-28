9eebd679212eb6de0cc514dc4321596311b97e4e
AlanAndres255 <perezlozanoalanandres@gmail.com>
2024-11-28 05:14:47 -0600

    Video en inicio, audio en blog y fix responsive

---
+++ src/pages/blog/js/audio.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+$(document).ready(function () {
+  const audio = new Audio('/public/blog_audio.mp3');
+  const playButton = $('#play-button');
+  const progressBar = $('#progress-bar');
+
+  let isPlaying = false;
+
+  playButton.on('click', function () {
+    if (isPlaying) {
+      audio.pause();
+      playButton.find('img').attr('src', 'https://cdn-icons-png.flaticon.com/512/727/727245.png'); 
+    } else {
+      audio.play();
+      playButton.find('img').attr('src', 'https://cdn-icons-png.flaticon.com/512/727/727269.png'); 
+    }
+    isPlaying = !isPlaying;
+  });
+
+  audio.addEventListener('timeupdate', function () {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.css('width', `${progress}%`);
+  });
+
+  audio.addEventListener('ended', function () {
+    progressBar.css('width', '0%');
+    playButton.find('img').attr('src', 'https://cdn-icons-png.flaticon.com/512/727/727245.png'); 
+    isPlaying = false;
+  });
+});
